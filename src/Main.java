ccabd958960e9c55dd16ee7b2716f3ad012dde2c
Brandon Smith <smithb@wlwv.k12.or.us>
2023-02-28 09:57:11 -0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names = {"Alice", "Bob", "Cam", "Don"};
+
+        for(int i = 0; i<names.length;i++){
+            names[i] = names[i] + " Smith";
+            System.out.println(names[i]);
+        }
+
+        for(String name: names){
+            name = name + " Smith";
+            System.out.println(name);
+        }
+
+        //It doesn't print Alice Smith Smith, Bob Smith Smith, etc.
+        for(String name: names){
+            System.out.println(name);
+        }
     }
 }
